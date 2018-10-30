f04f93c66d79029e257c140aae742c713726d5f4
Jonghyun Kim <jhkim@dit.ac.kr>
2018-10-30 13:40:24 +0900

    Initial Commit

---
+++ BusanMap02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusanMap02
+//
+//  Created by 김종현 on 30/10/2018.
+//  Copyright © 2018 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
