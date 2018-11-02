54a9819ab77167e7f7ddd07e840907fbee29e63d
Jonghyun Kim <jhkim@dit.ac.kr>
2018-11-02 22:15:11 +0900

    .

--- BusanMap02/ViewController.swift
+++ BusanMap02/ViewController.swift
@@ -83,14 +83,13 @@ class ViewController: UIViewController, MKMapViewDelegate, XMLParserDelegate {
         
         // Map
         myMapView.delegate = self
-        let location = CLLocationCoordinate2D(latitude: 35.180100, longitude: 129.081017)
-        let span = MKCoordinateSpan(latitudeDelta: 0.27, longitudeDelta: 0.27)
-        let region = MKCoordinateRegion(center: location, span: span)
-        myMapView.setRegion(region, animated: true)
+        
+        //  초기 맵 region 설정
+        zoomToRegion()
         
         for item in items {
             let dSite = item["site"]
-            print("dSite = \(dSite)")
+            print("dSite = \(String(describing: dSite))")
             
             // 추가 데이터 처리
             for (key, value) in addrs {
@@ -128,66 +127,17 @@ class ViewController: UIViewController, MKMapViewDelegate, XMLParserDelegate {
             annotation = BusanData(coordinate: CLLocationCoordinate2D(latitude: dLat!, longitude: dLong!), title: dSite!, subtitle: subtitleOut, pm10: dPM10!, pm10Cai: dPM10Cai!)
             
             annotations.append(annotation!)
-            //myMapView.showAnnotations(annotations, animated: true)
-            myMapView.addAnnotations(annotations)
+            myMapView.showAnnotations(annotations, animated: true)
+//            myMapView.addAnnotations(annotations)
   
         }
-        
-//        for addr in addrs {
-//            let stationVal = addr.value
-//            let stationName = addr.key
-////            print(stationName)
-//
-//            let address = stationVal[0]
-//            let lat = stationVal[1]
-//            let long = stationVal[2]
-//            let loc = stationVal[3]
-//
-//            let dLat = Double(lat)
-//            let dLong = Double(long)
-//
-//            tPM10 = "pm10 " + items[i]["pm10"]! + " ug/m2"
-//            //print(items[i]["pm10"])
-//
-//            i = i + 1
-//            annotation = BusanData(coordinate: CLLocationCoordinate2D(latitude: dLat!, longitude: dLong!), title: stationName, subtitle: tPM10!)
-//
-//            annotations.append(annotation!)
-//            myMapView.addAnnotations(annotations)
-//        }
-        
-        // Position the map such that the provided array of annotations are all visible to the fullest extent possible.
-//        myMapView.showAnnotations(annotations, animated: true)
-//        print(annotations.count)
-//        myMapView.addAnnotations(annotations)
-       
-        
-        
-//        let a = BusanData(coordinate: CLLocationCoordinate2D(latitude:35.0999630, longitude: 35.0999630), title: "광복동", subtitle: "광복동 주민센터")
-//        let b = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.0829920, longitude: 128.9668750), title: "장림동", subtitle: "사하여성회관")
-//        let c = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1460850, longitude: 128.9838270), title: "학장동", subtitle: "학장초등학교")
-//        let d = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2158660, longitude: 129.0197570), title: "덕천동", subtitle: "한국환경공단")
-//        let e = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1841140, longitude: 129.0786090), title: " 연산동", subtitle: "연제초등학교")
-//        let f = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1303210, longitude: 129.0876850), title: "대연동", subtitle: "부산공업고등학교")
-//        let g = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2752570, longitude: 129.0898810), title: "청룡동", subtitle: "청룡노포동 주민센터 옥상")
-//        let h = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1530480, longitude: 129.0635640), title: "전포동", subtitle: "경남공고 옥상")
-//        let i = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.0597260, longitude: 129.0798400), title: "태종대", subtitle: "태종대유원지관리사무소")
-//        let j = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2460560, longitude: 129.2118280), title: "기장읍", subtitle: "기장초등학교 옥상")
-//        let k = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2114600, longitude: 128.9547110), title: "대저동", subtitle: "대저차량사업소 옥상")
-//        let l = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2298390, longitude:  129.0927140), title: "부곡동", subtitle: "부곡2동 주민센터 옥상")
-//        let m = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1527040, longitude:  129.1078090), title: "광안동", subtitle: "구 보건환경연구원 3층")
-//        let n = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2047550, longitude:  129.1043270), title: "명장동", subtitle: "명장동 주민센터 옥상")
-//        let o = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.0953270, longitude:  128.8556680), title: "녹산동", subtitle: "(주)삼성전기부산사업장 옥상")
-//        let p = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.3255580, longitude:  129.1801400), title: "용수리", subtitle: "정관면 주민센터 2층 옥상")
-//        let q = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1708900, longitude:  129.1742250), title: "좌동", subtitle: "좌1동 주민센터 옥상")
-//        let r = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1293350, longitude:  129.0454230), title: "수정동", subtitle: "동구청사 옥상")
-//        let s = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.1173230, longitude:  129.0156410), title: "대신동", subtitle: "부산국민체육센터")
-//        let t = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.2056140, longitude:  129.0785020), title: "온천동", subtitle: "동래지하철 앞")
-//        let u = BusanData(coordinate: CLLocationCoordinate2D(latitude: 35.11194650, longitude:  129.0354560), title: "초량동", subtitle: "윤흥신장군 동상 앞")
-//
-//        myMapView.addAnnotations([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u])
-        //myMapView.delegate = self
-        
+    }
+    
+    func zoomToRegion() {
+        let location = CLLocationCoordinate2D(latitude: 35.180100, longitude: 129.081017)
+        let span = MKCoordinateSpan(latitudeDelta: 0.27, longitudeDelta: 0.27)
+        let region = MKCoordinateRegion(center: location, span: span)
+        myMapView.setRegion(region, animated: true)
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
